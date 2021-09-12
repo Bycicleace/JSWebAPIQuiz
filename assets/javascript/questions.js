e0f32a6efc8126f5440b02ba8de7390023d382ee
kvammee <elliott.kvamme@gmail.com>
2021-09-12 16:32:45 -0500

    Added Questions JS file for storing all questions

---
+++ assets/javascript/questions.js
@@ -0,0 +1,121 @@
+/*
+    This file is for storing all questions to be used in the quiz.
+    Each question should be defined, then added to the questionsArray
+*/
+
+questionsArray = [];
+
+var q1 = {
+    question: "Commonly used data types DO Not Include:",
+    answers: {
+        1: {
+            answer: "strings",
+            isCorrect: false
+        },
+        2: {
+            answer: "booleans",
+            isCorrect: false
+        },
+        3: {
+            answer: "alerts",
+            isCorrect: true
+        },
+        4: {
+            answer: "numbers",
+            isCorrect: false
+        }
+    }
+};
+questionsArray.push(q1);
+
+var q2 = {
+    question: "The condition in an if / else statement is enclosed with __________.",
+    answers: {
+        1: {
+            answer: "quotes",
+            isCorrect: false
+        },
+        2: {
+            answer: "curly brackets",
+            isCorrect: false
+        },
+        3: {
+            answer: "parenthesis",
+            isCorrect: true
+        },
+        4: {
+            answer: "square brackets",
+            isCorrect: false
+        }
+    }
+};
+questionsArray.push(q2);
+
+var q3 = {
+    question: "Arrays in JavaScript can be used to store __________.",
+    answers: {
+        1: {
+            answer: "numbers and strings",
+            isCorrect: false
+        },
+        2: {
+            answer: "other arrays",
+            isCorrect: false
+        },
+        3: {
+            answer: "booleans",
+            isCorrect: false
+        },
+        4: {
+            answer: "all of the above",
+            isCorrect: true
+        }
+    }
+};
+questionsArray.push(q3);
+
+var q4 = {
+    question: "String values must be enclosed within __________ when being assigned to variables.",
+    answers: {
+        1: {
+            answer: "commas",
+            isCorrect: false
+        },
+        2: {
+            answer: "curly brackets",
+            isCorrect: false
+        },
+        3: {
+            answer: "quotes",
+            isCorrect: true
+        },
+        4: {
+            answer: "parenthesis",
+            isCorrect: false
+        }
+    }
+};
+questionsArray.push(q4);
+
+var q5 = {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: {
+        1: {
+            answer: "JavaScript",
+            isCorrect: false
+        },
+        2: {
+            answer: "terminal/bash",
+            isCorrect: false
+        },
+        3: {
+            answer: "for loops",
+            isCorrect: false
+        },
+        4: {
+            answer: "console.log",
+            isCorrect: true
+        }
+    }
+};
+questionsArray.push(q5);
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,17 @@
 console.log("Script loaded.");
 
-// Set CSS to match styles
-// Create Questions (Collection of Objects)
-// Create Timer function
-// Create "Wrong" and "Correct" functions for footer
-// Create loadQuestion function
-// Create quizComplete function for when someone answers all questions
-// Add functionality for when the timer reaches 0.
-// Create scoring function to store score
-// Design High Score page
-// Add functionality to High Score buttons.
-// Add functionality to "View high scores" link
+/* TO DO
+ X | Set CSS to match styles
+   | Create Questions (Collection of Objects)
+   | Create Timer function
+   | Create "Wrong" and "Correct" functions for footer
+   | Create loadQuestion function
+   | Create quizComplete function for when someone answers all questions
+   | Add functionality for when the timer reaches 0.
+   | Create scoring function to store score
+   | Design High Score page
+   | Add functionality to High Score buttons.
+   | Add functionality to "View high scores" link
+*/
+
+console.log(questionsArray);
