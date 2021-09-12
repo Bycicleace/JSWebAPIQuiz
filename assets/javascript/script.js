74db732517e42fc92841860030eff84632fcfd5d
kvammee <elliott.kvamme@gmail.com>
2021-09-12 17:28:34 -0500

    Added LoadQuestion function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,3 +15,40 @@ console.log("Script loaded.");
 */
 
 console.log(questionsArray);
+
+// Loads the question at the index provided.
+var LoadQuestion = function(index) {
+    // Changes main-title to question
+    var mainTitle = document.querySelector("#main-title h2");
+    mainTitle.textContent = questionsArray[index].question;
+    mainTitle.setAttribute("style","text-align: left;");
+
+    // Changes main-subtitle to list of buttons, answers
+    var mainSubtitle = document.querySelector("#main-subtitle");
+    mainSubtitle.innerHTML = "";
+    mainSubtitle.setAttribute("style", "text-align: left;");
+
+    for (var i = 0; i < Object.keys(questionsArray[index].answers).length; i++) {
+        // Create answer button wrapper.
+        var answerDiv = document.createElement("div");
+        answerDiv.className = "answerDiv";
+
+        // Create answer button.
+        var answerButton = document.createElement("button");
+        answerButton.className = "answer";
+        answerButton.textContent = i + 1 + ". " + questionsArray[index].answers[i + 1].answer;
+        
+        // Put button in div
+        answerDiv.appendChild(answerButton);
+
+        // append div to main-subtitle
+        mainSubtitle.appendChild(answerDiv);
+    }
+
+    // clears out main-button
+    var mainButton = document.querySelector("#main-button");
+    if (mainButton) {
+        mainButton.remove();
+    }
+}
+
