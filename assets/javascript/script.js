62328100697ec6ccc45660363a24b3d2b64771d1
kvammee <elliott.kvamme@gmail.com>
2021-09-12 19:29:16 -0500

    various cleanup

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,9 @@
-console.log("Script loaded.");
-
 /* TO DO
  X | Set CSS to match styles
  X | Create Questions (Collection of Objects)
    | Create Timer function
    | Create "Wrong" and "Correct" functions for footer
-   | Create loadQuestion function
+ X | Create loadQuestion function
    | Create quizComplete function for when someone answers all questions
    | Add functionality for when the timer reaches 0.
    | Create scoring function to store score
@@ -13,8 +11,6 @@ console.log("Script loaded.");
    | Add functionality to High Score buttons.
    | Add functionality to "View high scores" link
 */
-
-console.log(questionsArray);
 
 // Loads the question at the index provided.
 var LoadQuestion = function(index) {
