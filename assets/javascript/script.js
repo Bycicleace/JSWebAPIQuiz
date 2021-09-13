4792274182b19e37fc6f6eb25ea720a83581c643
kvammee <elliott.kvamme@gmail.com>
2021-09-12 20:23:58 -0500

    Added timer functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,19 @@
 /* TO DO
  X | Set CSS to match styles
  X | Create Questions (Collection of Objects)
-   | Create Timer function
+ X | Create Timer function
    | Create "Wrong" and "Correct" functions for footer
  X | Create loadQuestion function
    | Create quizComplete function for when someone answers all questions
-   | Add functionality for when the timer reaches 0.
+ X | Add functionality for when the timer reaches 0.
    | Create scoring function to store score
    | Design High Score page
    | Add functionality to High Score buttons.
    | Add functionality to "View high scores" link
 */
+
+// Global variables
+var timerAmount = 0;
 
 // Loads the question at the index provided.
 var LoadQuestion = function(index) {
@@ -46,5 +49,63 @@ var LoadQuestion = function(index) {
     if (mainButton) {
         mainButton.remove();
     }
+};
+
+var StartTimer = function(amountOfSeconds) {
+    timerAmount = amountOfSeconds;
+    var timerElement = document.querySelector("#timer h2 span");
+    timerElement.textContent = timerAmount;
+    var timer = setInterval(function(){
+        timerAmount--;
+        timerElement.textContent = timerAmount;
+        if (parseInt(timerAmount) <= 0) {
+            clearInterval(timer);
+            GameOver();
+        }
+    }, 1000);
 }
 
+var GameOver = function() {
+    // Set game over screen.
+    var mainTitle = document.querySelector("#main-title h2");
+    mainTitle.textContent = "All done!";
+    
+    // Clear subtitle section
+    var mainSubtile = document.querySelector("#main-subtitle");
+    mainSubtile.innerHTML = "";
+
+    // Set subtitle section to current score
+    var subDiv = document.createElement("div");
+    subDiv.className = "alignLeft noPadding";
+    var subP = document.createElement("p");
+    subP.textContent = "Your final score is " + timerAmount;
+    subDiv.appendChild(subP);
+    mainSubtile.appendChild(subDiv);
+
+    // Use the main-button section for the Initial entry
+    var mainButton = document.querySelector("#main-button");
+
+    // Create a form to store the three elements
+    var form = document.createElement("form");
+
+    var labelInitials = document.createElement("label");
+    labelInitials.setAttribute("for", "textInitials");
+    labelInitials.textContent = "Enter initials:";
+
+    var textInitials = document.createElement("input");
+    textInitials.setAttribute("type", "text");
+    textInitials.setAttribute("id", "textInitials");
+    textInitials.setAttribute("name", "textInitials");
+
+    var submitButton = document.createElement("submit");
+    submitButton.className = "button";
+    submitButton.textContent = "Submit";
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault();
+    });
+
+    form.appendChild(labelInitials);
+    form.appendChild(textInitials);
+    form.appendChild(submitButton);
+    mainSubtile.appendChild(form);
+};
